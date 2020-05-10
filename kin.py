7dbd9b857090719d28bbc826d8612c1ad9ff8212
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-05-10 13:35:37 -0400

    tow p2

--- kin.py
+++ kin.py
@@ -7,6 +7,6 @@ mylabel1 = Label(root, text="hello")
 mylabel2 = Label(root, text="my name is ali")
 #shoving it on to screen
 mylabel1.grid(row=0, column=0)
-mylabel2.grid(row=1, column=0)
+mylabel2.grid(row=1, column=1)
 
 root.mainloop()
