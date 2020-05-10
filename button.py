2ffbbffa512de68e5318da439b730dd1f64bb704
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-05-10 13:58:39 -0400

    three p4

--- button.py
+++ button.py
@@ -2,7 +2,12 @@ from tkinter import *
 
 root = Tk()
 
-mybotton = Button(root, text='click', padx=50, pady=50)
+def myclick():
+    mylabel = Label(root, text='look!!!  click!')
+    mylabel.pack()
+
+
+mybotton = Button(root, text='click', command=myclick)
 mybotton.pack()
 
 root.mainloop()
