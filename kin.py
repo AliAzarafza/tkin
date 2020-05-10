5fd6d9c4632650f9c33ceedc805ba70c664d4cd4
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-05-10 13:33:40 -0400

    tow p1

--- kin.py
+++ kin.py
@@ -3,8 +3,10 @@ from tkinter import *
 root = Tk()
 
 #creat a label widget
-mylabel = Label(root, text="hello")
+mylabel1 = Label(root, text="hello")
+mylabel2 = Label(root, text="my name is ali")
 #shoving it on to screen
-mylabel.pack()
+mylabel1.grid(row=0, column=0)
+mylabel2.grid(row=1, column=0)
 
 root.mainloop()
