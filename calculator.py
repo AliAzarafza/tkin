ab3d9695d578399b07de0ca0f93ba5247cadea46
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-05-11 02:20:38 -0400

    five p2

--- calculator.py
+++ calculator.py
@@ -23,6 +23,10 @@ button_7 = Button(root, text="7", padx=35, pady=20, command=ButtonAdd)
 button_8 = Button(root, text="8", padx=35, pady=20, command=ButtonAdd)
 button_9 = Button(root, text="9", padx=35, pady=20, command=ButtonAdd)
 button_0 = Button(root, text="0", padx=35, pady=20, command=ButtonAdd)
+button_add = Button(root, text="+", padx=34, pady=20, command=ButtonAdd)
+button_eqale = Button(root, text="=", padx=80, pady=20, command=ButtonAdd)
+button_clear = Button(root, text="clear", padx=68, pady=20, command=ButtonAdd)
+
 
 #put buttons on screen
 button_1.grid(row=3, column=0)
@@ -38,6 +42,10 @@ button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+
+button_add.grid(row=5, column=0)
+button_eqale.grid(row=5, column=1, columnspan=2)
 
 
 
