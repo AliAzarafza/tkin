372516faf6a951776656e8162fa35f92a4954fe8
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-05-11 04:02:48 -0400

    six p2

--- use icon.py
+++ use icon.py
@@ -1,10 +1,18 @@
 from tkinter import *
-
+from PIL import ImageTk,Image
 root = Tk()
 root.title('lern for your self')
 root.iconbitmap('C:/Users/Ali/Pictures/gigapixel.ico')
 
+myimege = ImageTk.PhotoImage(Image.open('fe96b10857930eefdf79d682bff35c50.jpg'))
+mylabel =Label(image=myimege )
+mylabel.pack()
 
+
+
+
+ButtonQuit= Button(root, text='exit', command=root.quit)
+ButtonQuit.pack()
 
 
 
